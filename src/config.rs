545a434ca91147c502bedb3656217510ccb19e02
Ini Etienam <ietienam@gmail.com>
2024-11-11 21:27:51 +0100

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -3,5 +3,5 @@ use std::env;
 
 pub fn get_database_url() -> String {
   dotenv().ok();
-  env::var("DATABASE_URL").expect("DATABASE_URL must be set")
+  env::var("DATABASE_URL").expect("DATABASE_URL must be set!")
 }
